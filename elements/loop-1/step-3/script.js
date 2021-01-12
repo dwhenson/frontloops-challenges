eba2c6a664e3c6a142d1f20c73d021eefd479ce8
dwhenson <dave.henson@gmail.com>
2021-01-12 22:18:27 +0300

    📦 ADD:  javascript and finalise CSS

--- elements/loop-1/step-3/script.js
+++ elements/loop-1/step-3/script.js
@@ -1,1 +1,22 @@
-console.log("It works");
+const toggles = [...document.querySelectorAll(".slider")];
+const list = document.querySelector("ul");
+
+function randomBoolean() {
+	return Math.random() < 0.5;
+}
+
+function clickHandler(event) {
+	const uncheckedToggles = toggles.filter(
+		(toggle) => toggle.index !== event.target.id
+	);
+	uncheckedToggles.forEach((toggle) => {
+		if (randomBoolean()) {
+			toggle.previousElementSibling.checked
+				? toggle.previousElementSibling.removeAttribute("checked")
+				: toggle.previousElementSibling.setAttribute("checked", true);
+		}
+	});
+	console.log(uncheckedToggles, toggles);
+}
+
+list.addEventListener("click", clickHandler);
