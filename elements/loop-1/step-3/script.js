9f758dd4c1434397e0df089a60ecb4c518b42f7c
dwhenson <dave.henson@gmail.com>
2021-01-14 18:08:00 +0000

    ✨IMPROVE:  JS loop-1, step-3 solution

--- elements/loop-1/step-3/script.js
+++ elements/loop-1/step-3/script.js
@@ -4,13 +4,21 @@ const list = document.querySelector("ul");
 function randomBoolean() {
 	return Math.random() < 0.5;
 }
+//
+// function clickHandler(event) {
+// 	// if (!event.target.closest(".toggles")) return;
+// 	const filtered = toggles.filter((item) => item.id !== event.target.id);
+// 	filtered.forEach((input) => {
+// 		randomBoolean() && (input.checked = !input.checked);
+// 	});
+// }
 
 function clickHandler(event) {
-	// if (!event.target.closest(".toggles")) return;
-	const filtered = toggles.filter((item) => item.id !== event.target.id);
-	filtered.forEach((input) => {
-		randomBoolean() && (input.checked = !input.checked);
+	if (!event.target.closest(".toggles")) return;
+	toggles.forEach((input) => {
+		if (input === event.target) return;
+		input.checked = randomBoolean();
 	});
 }
 
-list.addEventListener("click", clickHandler);
+list.addEventListener("change", clickHandler);
