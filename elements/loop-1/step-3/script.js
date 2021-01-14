773a509c218aaa268cb68cb0e67aed00a7422def
dwhenson <dave.henson@gmail.com>
2021-01-14 19:18:34 +0000

    🧼 REFACTOR:  JS solution elements loop-1, step-3

--- elements/loop-1/step-3/script.js
+++ elements/loop-1/step-3/script.js
@@ -4,14 +4,6 @@ const list = document.querySelector("ul");
 function randomBoolean() {
 	return Math.random() < 0.5;
 }
-//
-// function clickHandler(event) {
-// 	// if (!event.target.closest(".toggles")) return;
-// 	const filtered = toggles.filter((item) => item.id !== event.target.id);
-// 	filtered.forEach((input) => {
-// 		randomBoolean() && (input.checked = !input.checked);
-// 	});
-// }
 
 function clickHandler(event) {
 	if (!event.target.closest(".toggles")) return;
