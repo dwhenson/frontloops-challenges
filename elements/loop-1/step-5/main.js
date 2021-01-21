a612cbdaa6f27dda48d87cc42490200b49b925fe
dwhenson <dave.henson@gmail.com>
2021-01-21 17:42:59 +0300

    📦 ADD: Files for Markup loop-1, step-7

---
+++ elements/loop-1/step-5/main.js
@@ -0,0 +1,5 @@
+// Create a string of text that simulates auto-type effect.
+// After each word is typed - there should be a pause, before starting typing the next one.
+// All the words variants should be set in the javascript code.
+
+console.log("it works");
