f1b051d731c958e6bfb6dda153388f32b4eaa938
dwhenson <dave.henson@gmail.com>
2021-01-22 17:11:37 +0300

    ✅ DONE: Final solution Elements loop-1, step-5

--- elements/loop-1/step-5/main.js
+++ elements/loop-1/step-5/main.js
@@ -2,4 +2,23 @@
 // After each word is typed - there should be a pause, before starting typing the next one.
 // All the words variants should be set in the javascript code.
 
-console.log("it works");
+const words = ["design", "development", "frontend", "backend"];
+const placeholder = document.querySelector("#typer");
+
+function wait(ms = 0) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function updateText() {
+	for (const word of words) {
+		placeholder.textContent = "";
+		const letters = [...word];
+		for (const letter of letters) {
+			placeholder.textContent += letter;
+			await wait(20);
+		}
+		await wait(word.length * 10);
+	}
+}
+
+updateText();
